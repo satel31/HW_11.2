7e616f18cf8b2464a5c6b82185d7963ef47be435
satel31 <blokhnina.tatiana@yandex.ru>
2023-02-11 23:11:00 +0300

    Added capitalize_letters function and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def upper_letters(string):
+    """Capitalizes all letters of a string
+    Returns a string"""
     return string.upper()
+
+def capitalize_letters(string):
+    """Capitalizes the first letter of a string
+    Returns a string"""
+    return string.capitalize()
