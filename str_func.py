d983194ed01ff19201ba9a39157b7ab2927530b6
satel31 <blokhnina.tatiana@yandex.ru>
2023-02-11 23:34:39 +0300

    Changed the code

--- str_func.py
+++ str_func.py
@@ -1,9 +1,13 @@
-def upper_letters(string):
+def upper_letters(string) -> str:
     """Capitalizes all letters of a string
     Returns a string"""
     return string.upper()
 
-def capitalize_letters(string):
+def capitalize_letters(string) -> str:
     """Capitalizes the first letter of a string
     Returns a string"""
     return string.capitalize()
+
+
+print(upper_letters('This is not a bug'))
+print(capitalize_letters('This is a feature'))
